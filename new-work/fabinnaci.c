e8f79347989046121335e829526d864b7d778b74
Mustafa-Hassan2001 <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2022-03-16 22:51:23 +0500

    Add files via upload

---
+++ new-work/fabinnaci.c
@@ -0,0 +1,549 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+//	int i , j ;
+//	for(i=1;i<=5;i+=2){
+//		for(j=1;j<=i;j+=2){
+//			printf("%d",j);
+//		}
+//		printf("\n");
+//	}
+
+
+//int i , j;
+//for(i=1;i<=5;i++){
+//	for(j=1;j>=i;j+=2){
+//		printf("%d",j);
+//	}
+//	printf("\n");
+//}
+
+
+
+//int i=4 , j=12;
+//if(i=5||j>50){
+//	printf("hello");
+//}else{
+//	printf("hi");
+//}
+
+
+
+//int i=4 ,j=-1 ,k=0 ,w;
+//w=i||j||k;
+//printf("%d",w);
+
+
+//int i ,  factorial=1 , num ;
+//
+//printf("Enter number:");
+//scanf("%d",&num); 
+//
+//for(i=1;i<=num;i++){
+//	factorial = factorial * i;
+//}
+//
+//printf("The Factorial of %d  is: %d", num , factorial);
+	
+	
+
+//	int num,i;
+//	scanf("%d",&num);
+//	for(i=1;i<=num;i++){
+//	printf("the number is %d cube of %d is : %d.\n",i , i ,(i*i*i));
+//    }
+	
+
+
+
+//int i ;
+//for(i=1;i<=10;i++){
+//	printf("15*%d=%d\n",i,(15*i));
+//}	
+
+	
+//int j,i,n;
+//   printf("Input upto the table number starting from 1 : ");
+//   scanf("%d",&n);
+//   printf("Multiplication table from 1 to %d \n",n);
+//   for(i=1;i<=10;i++)
+//   {
+//     for(j=1;j<=n;j++)
+//     {
+//      // if (j<=n-1)
+//           printf("%dx%d = %d,\t ",j,i,i*j);
+//       //   else
+//	  //\0  printf("%dx%d = %d \t",j,i,i*j);
+//
+//      }
+//     printf("\n");
+//    }	
+
+
+
+
+	
+//int i , j , num;
+//scanf("%d",&num);
+//for(i=1;i<=10;i++){
+//	for(j=1;j<=num;j++){
+//		printf("%d x %d = %d\t",i,j,(j*i));
+//	}
+//	printf("\n");
+//}	
+	
+	
+
+//int i ,j ,num ,sum=0;
+//printf("Enter Number:");
+//scanf("%d",&num);	
+//	for(i=1;i<=num;i+=2){
+//		printf("%d,",i);
+//		sum=sum+i;
+//	}
+//	printf("\nThe sum of odd num is : %d",sum);
+//	
+	
+	
+//int i,j;
+//for(i=1;i<=5;i++){
+//	for(j=1;j<=i;j++){
+//		printf("*");
+//	}
+//	printf("\n");
+//}	
+
+
+//int i ,j ;
+//for(i=1 ; i<=5 ; i++){
+//	for(j=1;j<=i;j++){
+//	printf("%d",i);
+//   }
+//    printf("\n");
+//}
+
+
+//int i , j , row , k=1 ;
+//printf("Input number of rows : ");
+//scanf("%d",&row);
+//for(i=1;i<=row;i++)
+//{
+//	for(j=1;j<=i;j++){
+//	    	printf("%d ",k++);
+//	    }
+//		printf("\n");
+//}
+
+//int i,j,spc,rows,k;
+//   printf("Input number of rows : ");
+//   scanf("%d",&rows);
+//   spc=rows+4-1;
+//   for(i=1;i<=rows;i++)
+//   {
+//         for(k=spc;k>=1;k--)
+//            {
+//              printf(" ");
+//            }
+//                      
+//	   for(j=1;j<=i;j++)
+//	   printf("* ");
+//	printf("\n");
+//    spc--;
+//}
+
+//int i,j,spc,rows,k;
+//   printf("Input number of rows : ");
+//   scanf("%d",&rows);
+//   spc=rows+4-1;
+//   for(i=1;i<=rows;i++)
+//   {
+//         for(k=spc;k>=1;k--)
+//            {
+//              printf(" ");
+//            }
+//                      
+//	   for(j=1;j<=i;j++){
+//	   
+//	   printf("%d ",i);
+//}
+//	printf("\n");
+//    spc--;
+//}
+
+
+
+
+
+//int i , num , sum=0;
+//printf("Enter number=");
+//scanf("%d",&num);
+//for(i=2;i<=num;i++){
+//	printf("%d,",2*i);
+//	sum+=2*i;	
+//}
+//printf("\nThe sum of even natural number form %d is: %d", num , sum);
+
+
+//int i , num , factorial=1;
+//printf("Enter number=");
+//scanf("%d",&num);
+//for(i=1;i<=num;i++){
+//	factorial*=i;
+//}
+//printf("The factorial of %d is: %d", num , factorial);
+//
+
+
+//int num ,i ;
+//float sum=0;
+// printf("Enter number=");
+// scanf("%d",&num);
+// printf("\n\n");
+// for(i=1;i<=num;i++){
+// 	if(i<=num){
+// 		printf("1/%d+",i);
+// 		sum+=1/(float)i;
+//     }
+// }
+// printf("\n The Sum of %d term is : %f", num , sum);
+
+
+
+
+
+// int i,j,n;
+//   printf("Input number of rows for this pattern :");
+//   scanf("%d",&n);
+//   for(i=0;i<n;i++)
+//   {
+//     for(j=1;j<=n-i;j++)
+//     printf(" ");
+//     for(j=1;j<=2*i-1;j++)
+//       printf("*");
+//     printf("\n");
+//   }
+//
+
+
+//int i ,j ,num;
+//printf("Entr number:");
+//scanf("%d",num);
+//for(i=0;i<=num;i++){
+//	for(j=1;j<=num-i;j++)
+//	printf(" ");
+//	for(j=1;j<=i;j++){
+//		printf("*");
+//	}
+//	printf("\n");
+//}
+
+
+
+
+
+
+
+
+
+
+
+//int i , j , num;
+//printf("Enter number=");
+//scanf("%d",&num);
+//for(i=0;i<=num;i++){
+//	for(j=1;j<=num-i;j++)
+//	printf(" ");
+//	for(j=1;j<=2*i-1;j++){
+//		printf("*");
+//	}
+//	printf("\n");
+//}
+
+
+
+
+
+
+
+
+
+
+//int i ,j ,num;
+//printf("Enter number for rows=");
+//scanf("%d",&num);
+//for(i=0;i<=num;i++)
+//{	
+//	for(j=1;j<=num-i;j++)
+//		   printf(" ");
+//		for(j =1;j<=2*i-1;j++){
+//			printf("*");
+//		}
+//         printf("\n");
+//}
+
+
+
+
+
+//   int i,j,r;
+//   printf("Input number of rows (half of the diamond) :");
+//   scanf("%d",&r);
+//   for(i=0;i<=r;i++)
+//   {
+//     for(j=1;j<=r-i;j++)
+//     printf(" ");
+//     for(j=1;j<=2*i-1;j++)
+//       printf("*");
+//     printf("\n");
+//   }
+// 
+//   for(i=r-1;i>=1;i--)
+//   {
+//     for(j=1;j<=r-i;j++)
+//     printf(" ");
+//     for(j=1;j<=2*i-1;j++)
+//       printf("*");
+//     printf("\n");
+//   }
+ 
+
+
+
+
+
+
+//int i , j ,space,row ,k;
+//printf("Enter number ");
+//scanf("%d",&row);
+//space=row+4-1;
+//for(i=1;i<=row;i++)
+//{
+//	for(k=space;k>=1;k--)
+//    {
+//		printf(" ");
+//	}
+//
+//	for(j=1;j=i;j++)
+//		printf("%d ",i);
+//	printf("\n");
+//	space--;
+//}
+
+
+//
+//  int i,j,spc,rows,k;
+//   printf("Input number of rows : ");
+//   scanf("%d",&rows);
+//   spc=rows+4-1;
+//   for(i=1;i<=rows;i++)
+//   {
+//         for(k=spc;k>=1;k--)
+//            {
+//              printf(" ");
+//            }
+//                      
+//	   for(j=1;j<=i;j++)
+//	   printf("%d ",i);
+//	printf("\n");
+//    spc--;
+//   }
+//
+
+
+
+
+//int i ,j ,n=5;
+//for(i=1;i<=n;i++){
+//	for(j=1;j<=i;j++){
+//		printf("%d",j+2);
+//	}
+//	printf("\n");
+//}
+
+
+
+int reminder , reverse , num=12345;
+while(num!=0){
+	reminder=num%10;
+	reverse=reverse*10+reminder;
+	num=num/10;
+}
+
+
+//int i,j;
+//for(i=1;i<=5;i++){
+//	for(j=0;j<=i;j++){
+//		printf("%d",j*2);
+//	}
+//	printf("\n");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int i,j,n;
+//   printf("Input number of rows for this pattern :");
+//   scanf("%d",&n);
+//   for(i=0;i<n;i++)
+//   {
+//     for(j=1;j<=n-i;j++)
+//     printf(" ");
+//     for(j=1;j<=2*i-1;j++)
+//       printf("*");
+//     printf("\n");
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+//int i ,j ,n;
+//printf("Enter number=");
+//scanf("%d",&n);
+//for(i=1;i<=n;i++){
+//	for(j=i;j>=1--){
+//		
+//		printf("%d",j%2);
+//	}
+//	printf("\n");
+//}
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	return 0;
+}
